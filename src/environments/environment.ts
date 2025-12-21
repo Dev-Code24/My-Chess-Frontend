3ca6a0b608b582feb9eff2e2a328e60e6f060655
Dev Code <hellodev_code@outlook.com>
2025-12-21 23:24:31 +0530

    Changes: • Added vercel.json • Added prod backend link

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ import { EnvironmentConfigurations } from "@shared/@interface";
 
 export const environment: EnvironmentConfigurations = {
   environment: 'prod',
-  baseApiUrl: '',
+  baseApiUrl: 'https://my-chess-backend-784046893674.us-central1.run.app',
 };
