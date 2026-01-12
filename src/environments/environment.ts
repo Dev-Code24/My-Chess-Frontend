d4a978fc5f878f33c5f85bd845502c1c1b8e32e9
Dev Code <hellodev_code@outlook.com>
2026-01-13 00:45:44 +0530

    Changes: • Updated prod backend url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ import { EnvironmentConfigurations } from "@shared/@interface";
 
 export const environment: EnvironmentConfigurations = {
   environment: 'prod',
-  baseApiUrl: 'https://my-chess-backend-784046893674.us-central1.run.app',
+  baseApiUrl: 'https://my-chess-backend-784046893674.asia-south1.run.app',
 };
